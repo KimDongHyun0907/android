df8c5cc9ee169c6713acc9cf41d23013bee79aea
KimDongHyun <88826811+KimDongHyun0907@users.noreply.github.com>
2022-12-09 23:36:47 +0900

    Update MainActivity.java

--- LocationServiceTest/MainActivity.java
+++ LocationServiceTest/MainActivity.java
@@ -26,9 +26,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.textView);
 
-        if((ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED) &&
-                (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED)){
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION);
+        if((ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= 
+            PackageManager.PERMISSION_GRANTED) &&
+                (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)!= 
+                 PackageManager.PERMISSION_GRANTED)){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, 
+                                                                 Manifest.permission.ACCESS_COARSE_LOCATION}, 
+                                              REQUEST_LOCATION);
         }
 
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
@@ -36,8 +40,8 @@ public class MainActivity extends AppCompatActivity {
         LocationListener locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(@NonNull Location location) {
-                textView.setText("위도: " + location.getLatitude() + "\n경도: " + location.getLongitude() + "\n고도: " + location.getAltitude()
-                + "\n속도: " + location.getSpeed());
+                textView.setText("위도: " + location.getLatitude() + "\n경도: " + location.getLongitude() + 
+                                 "\n고도: " + location.getAltitude() + "\n속도: " + location.getSpeed());
             }
         };
 
